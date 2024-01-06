e219976c9ad290f9fc160e3b1455f667ee888526
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-06 23:47:13 +0800

    setup workflow development

---
+++ libs/__mocks__/prisma.ts
@@ -0,0 +1,13 @@
+// 1 imports
+import {PrismaClient} from "@prisma/client";
+import {beforeEach} from "vitest";
+import {mockDeep, mockReset} from "vitest-mock-extended";
+
+// 2 reset mock each test
+beforeEach(() => {
+	mockReset(prisma);
+});
+
+// 3 deeply mock all nested object you have
+const prisma = mockDeep<PrismaClient>();
+export default prisma;
