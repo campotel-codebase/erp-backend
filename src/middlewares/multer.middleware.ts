a907f7083c9782e06c7eeb3566943322f6642e59
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-08 01:21:08 +0800

    setup multer

---
+++ src/middlewares/multer.middleware.ts
@@ -0,0 +1,31 @@
+import multer from "multer";
+import {generateUuid} from "../utils/uuid.util";
+
+const MIME_TYPE_MAP: {[key: string]: string} = {
+	"image/png": "png",
+	"image/jpeg": "jpg",
+	"image/jpg": "jpg",
+};
+
+const imageStorage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "public/avatar/");
+	},
+	filename: async function (req, file, cb) {
+		const uuid = await generateUuid();
+		const ext = file.originalname.split(".").pop();
+		const fileName = `${uuid}.${ext}`;
+		cb(null, fileName);
+	},
+});
+
+export const uploadImage = multer({
+	storage: imageStorage,
+	fileFilter: (req, file, cb) => {
+		if (MIME_TYPE_MAP[file.mimetype]) {
+			cb(null, true);
+		} else {
+			cb(null, false);
+		}
+	},
+});
