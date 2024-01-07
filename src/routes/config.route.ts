d57a7a4e4f78f63eb6d943982eac17b941ac8891
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-07 16:05:30 +0800

    company configuration

--- src/routes/config.route.ts
+++ src/routes/config.route.ts
@@ -1,13 +1,17 @@
 import express from "express";
-import {setDepartments} from "../functions/config.function";
+import {
+	setBenefits,
+	setDepartments,
+	setJobTitles,
+	setTalentSegments,
+} from "../functions/config.function";
 const config = express.Router();
 
 config.patch("/departments", async (req, res) => {
 	const currentUser = req.authorization;
-	const body = req.body;
-	const departmentsToString = JSON.stringify(body.departments);
+	const toString = JSON.stringify(req.body.departments);
 	const prepData = {
-		departments: departmentsToString,
+		departments: toString,
 	};
 	try {
 		const result = await setDepartments(prepData, currentUser.companyUuid);
@@ -16,8 +20,44 @@ config.patch("/departments", async (req, res) => {
 		res.status(500).json(error.message);
 	}
 });
-config.post("/job-titles", (req, res) => {});
-config.post("/talent-segments", (req, res) => {});
-config.post("/benefits", (req, res) => {});
+config.patch("/job-titles", async (req, res) => {
+	const currentUser = req.authorization;
+	const toString = JSON.stringify(req.body.jobTitles);
+	const prepData = {
+		jobTitles: toString,
+	};
+	try {
+		const result = await setJobTitles(prepData, currentUser.companyUuid);
+		res.status(result.status).json(result.data);
+	} catch (error: any) {
+		res.status(500).json(error.message);
+	}
+});
+config.patch("/talent-segments", async (req, res) => {
+	const currentUser = req.authorization;
+	const toString = JSON.stringify(req.body.talentSegments);
+	const prepData = {
+		talentSegments: toString,
+	};
+	try {
+		const result = await setTalentSegments(prepData, currentUser.companyUuid);
+		res.status(result.status).json(result.data);
+	} catch (error: any) {
+		res.status(500).json(error.message);
+	}
+});
+config.patch("/benefits", async (req, res) => {
+	const currentUser = req.authorization;
+	const toString = JSON.stringify(req.body.benefits);
+	const prepData = {
+		benefits: toString,
+	};
+	try {
+		const result = await setBenefits(prepData, currentUser.companyUuid);
+		res.status(result.status).json(result.data);
+	} catch (error: any) {
+		res.status(500).json(error.message);
+	}
+});
 
 export default config;
