e093925bbe2f731d66899a6e2b6bb2d99aa415ee
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-10 16:56:35 +0800

    forgot password

--- src/functions/portal/employee.function.ts
+++ src/functions/portal/employee.function.ts
@@ -6,6 +6,21 @@ import {
 	sendResetLinkForPwd,
 } from "../../utils/password.util";
 
-export const employeeSignIn = async () => {};
-export const employeePwdResetLink = async () => {};
-export const employeeResetPwd = async () => {};
+export const employeeSignIn = async (body: employeeSignInType) => {
+	return {status: 404, data: ""};
+};
+export const employeePwdResetLink = async (email: string) => {
+	const isExists = await prisma.employee.findUnique({
+		where: {email},
+		select: {uuid: true},
+	});
+	if (isExists) {
+		await sendResetLinkForPwd(email, "employee");
+		return {status: 200, data: "password reset link was sent to your email address"};
+	} else {
+		return {status: 404, data: "email not found"};
+	}
+};
+export const employeeResetPwd = async (body: {uuid: string; newPassword: string}) => {
+	return {status: 404, data: ""};
+};
--- src/routes/public.route.ts
+++ src/routes/public.route.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import {userSignIn, userSignUp, userPwdResetLink, userResetPwd} from "../functions/user.function";
+import {employeePwdResetLink, employeeSignIn} from "../functions/portal/employee.function";
 const publicRoute = express.Router();
 
 publicRoute.post("/user/sign-up", async (req, res) => {
@@ -34,6 +35,8 @@ publicRoute.post("/user/reset-password", async (req, res) => {
 		res.status(500).json({error: error.message});
 	}
 });
+
+// Portal
 publicRoute.post("/employee/sign-in", async (req, res) => {
 	try {
 	} catch (error: any) {
@@ -42,6 +45,8 @@ publicRoute.post("/employee/sign-in", async (req, res) => {
 });
 publicRoute.post("/employee/forgot-password", async (req, res) => {
 	try {
+		const result = await employeePwdResetLink(req.body.email);
+		res.status(result.status).json(result.data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
