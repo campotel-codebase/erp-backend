f5bd6e1b05f2c9e0f1686733e1f32eb32c10df2d
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-13 11:50:12 +0800

    updated middlware

--- src/middlewares/authorization.middleware.ts
+++ src/middlewares/authorization.middleware.ts
@@ -1,3 +1,4 @@
+import prisma from "../../libs/prisma";
 import {Request, Response, NextFunction} from "express";
 import jwt from "jsonwebtoken";
 import {jwtPayloadType} from "../../types/jwt-payload";
@@ -8,15 +9,38 @@ declare module "express-serve-static-core" {
 	}
 }
 
-export const authorization = (req: Request, res: Response, next: NextFunction) => {
+export const authorization = async (req: Request, res: Response, next: NextFunction) => {
 	const secret = process.env.AUTH_KEY;
 	const authHeader = req.headers.authorization;
 	if (secret) {
 		if (authHeader) {
 			const token = authHeader.split(" ")[1];
-			const tokenProperties: any = jwt.verify(token, secret);
-			req.authorization = tokenProperties;
-			next();
+			const tokenProperties = jwt.verify(token, secret);
+			const tokenPayload = tokenProperties as jwtPayloadType;
+			try {
+				const validatePayload = await prisma.company.findUniqueOrThrow({
+					where: {uuid: tokenPayload.companyUuid},
+					select: {
+						uuid: true,
+						User: {
+							where: {
+								uuid: tokenPayload.userUuid,
+							},
+							select: {
+								uuid: true,
+							},
+						},
+					},
+				});
+				const prepData: jwtPayloadType = {
+					companyUuid: validatePayload.uuid,
+					userUuid: validatePayload.User[0].uuid,
+				};
+				req.authorization = prepData;
+				next();
+			} catch (error: any) {
+				res.status(401).json({error: error.message});
+			}
 		} else {
 			res.status(401).json("undefined auth header");
 		}
