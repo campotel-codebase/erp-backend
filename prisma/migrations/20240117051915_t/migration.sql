2018e5d893faf0dc7de5a32c7b7f56d381922e37
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-17 13:38:46 +0800

    leaveRequest schema

---
+++ prisma/migrations/20240117051915_t/migration.sql
@@ -0,0 +1,29 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `resume` on the `LeaveRequest` table. All the data in the column will be lost.
+  - Added the required column `CustomRoutingApproval` to the `LeaveRequest` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `resumeOn` to the `LeaveRequest` table without a default value. This is not possible if the table is not empty.
+
+*/
+BEGIN TRY
+
+BEGIN TRAN;
+
+-- AlterTable
+ALTER TABLE [dbo].[LeaveRequest] DROP COLUMN [resume];
+ALTER TABLE [dbo].[LeaveRequest] ADD [CustomRoutingApproval] VARCHAR(255) NOT NULL,
+[resumeOn] DATETIME2 NOT NULL;
+
+COMMIT TRAN;
+
+END TRY
+BEGIN CATCH
+
+IF @@TRANCOUNT > 0
+BEGIN
+    ROLLBACK TRAN;
+END;
+THROW
+
+END CATCH
