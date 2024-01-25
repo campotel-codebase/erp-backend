db36a66cf3d67fb63719551e5209d1767290c6ed
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-25 16:12:58 +0800

    new patern

---
+++ src/functions/modules/hris/post.hris.function.ts
@@ -0,0 +1,153 @@
+import {Prisma} from "@prisma/client";
+import {parse} from "csv-parse";
+import {userAuthCredentialsType} from "../../../../types/jwt-payload";
+import {generatePassword, hashPassword} from "../../../utils/password.util";
+import prisma from "../../../../libs/prisma";
+import {formatISO} from "date-fns";
+import {generateUuid} from "../../../utils/uuid.util";
+import {emailContent} from "../../../utils/email.util";
+
+export const importEmployees = async (
+	company: userAuthCredentialsType["company"],
+	csvBuffer: string,
+) => {
+	const benefitsToArray: string[] = JSON.parse(company.benefits ? company.benefits : "[]");
+	const expectedHeader = [
+		"lastName",
+		"firstName",
+		"middleName",
+		"suffix",
+		"phoneNumber",
+		"nickname",
+		"email",
+		"birthday",
+		"bloodType",
+		"driverLicense",
+		"taxId",
+		...benefitsToArray,
+	];
+
+	const parseCsv = await new Promise((resolve, reject) => {
+		parse(
+			csvBuffer,
+			{
+				columns: true,
+				delimiter: ",",
+				skip_empty_lines: true,
+				skip_records_with_empty_values: true,
+				on_record: (record, lines: any) => {
+					const csvHeaders = lines.columns.map((column: {name: string}) => column.name);
+
+					// INFO - compare headers
+					const isHeaderValid =
+						csvHeaders.length === expectedHeader.length &&
+						csvHeaders.every((value: string) => expectedHeader.includes(value));
+					if (!isHeaderValid) {
+						return {status: 400, data: "invalid csv header"};
+					}
+
+					// INFO - Convert empty strings to null
+					Object.keys(record).forEach((key) => {
+						if (record[key] === "") {
+							record[key] = null;
+						}
+					});
+
+					record.fullName = `${record.lastName} ${record.middleName} ${record.firstName}`;
+					return record;
+				},
+			},
+			(err: any, data) => {
+				if (err) {
+					reject(err);
+				} else {
+					resolve(data);
+				}
+			},
+		);
+	});
+	return {status: 200, data: parseCsv};
+};
+
+export const makeEmployees = async (
+	company: userAuthCredentialsType["company"],
+	body: Prisma.EmployeeCreateManyInput[],
+) => {
+	const employees = await Promise.all(
+		body.map(async (employee: Prisma.EmployeeCreateManyInput) => {
+			const {department, jobTitle, talentSegment, benefits, hiredDate, ...rest} = employee;
+			const fullName = `${employee.lastName} ${employee.firstName} ${employee.middleName}`;
+			const benefitsToString = JSON.stringify(benefits);
+			return {
+				...rest,
+				companyId: company.id,
+				fullName,
+				hiredDate,
+				benefits: benefitsToString,
+				uuid: await generateUuid(),
+				password: generatePassword,
+			};
+		}),
+	);
+	const hashPasswords = await Promise.all(
+		employees.map(async (employee: Prisma.EmployeeCreateManyInput) => {
+			const {password, ...rest} = employee;
+			return {
+				...rest,
+				password: await hashPassword(password),
+			};
+		}),
+	);
+	const newEmployees = await prisma.employee.createMany({
+		data: hashPasswords,
+	});
+	for (const employee of employees) {
+		const sendTo = {
+			to: employee.email,
+			subject: "newly hired",
+			text: {
+				title: `Welcome ${employee.fullName} to ${company.name}`,
+				msg: `Temporary ERP portal password: ${employee.password}`,
+			},
+			usedFor: "notification",
+		};
+		await emailContent(sendTo);
+	}
+	return {status: 200, data: newEmployees};
+};
+
+export const makeEmployee = async (
+	company: userAuthCredentialsType["company"],
+	body: {employee: Prisma.EmployeeCreateInput; reportingToId: number},
+) => {
+	const fullName = `${body.employee.lastName} ${body.employee.firstName} ${body.employee.middleName}`;
+	const {benefits, hiredDate, ...rest} = body.employee;
+	const benefitsToString = JSON.stringify(benefits);
+	const tempPassword = generatePassword;
+
+	const newEmployee = await prisma.employee.create({
+		data: {
+			...rest,
+			fullName,
+			hiredDate: formatISO(hiredDate),
+			benefits: benefitsToString,
+			password: await hashPassword(tempPassword),
+			uuid: await generateUuid(),
+			Company: {connect: {id: company.id}},
+			ReportingTo: {
+				connect: {id: body.reportingToId},
+			},
+		},
+	});
+	const sendTo = {
+		to: newEmployee.email,
+		subject: "newly hired",
+		text: {
+			title: `Welcome ${newEmployee.fullName} to ${company.name}`,
+			msg: `temporary erp portal password: ${tempPassword}`,
+		},
+		usedFor: "notification",
+	};
+	await emailContent(sendTo);
+	return {status: 200, data: {newEmployee, tempPassword}};
+};
--- src/routes/modules/hris.route.ts
+++ src/routes/modules/hris.route.ts
@@ -1,163 +1,179 @@
 import express from "express";
-import {uploadCsv} from "../../middlewares/multer.middleware";
 import {
-	assignBankAccount,
-	onBoardEmployees,
-	employee,
-	employees,
-	employeesCsvToJsonArray,
-	offboardEmployee,
-	onboardEmployee,
-	orgChartTree,
-	searchEmployee,
-	updateEmployee,
-	updateEmploymentHistory,
-	updateBankAccount,
-} from "../../functions/modules/hris.function";
+	findEmployee,
+	getEmployee,
+	getEmployees,
+} from "../../functions/modules/hris/get.hris.function";
 import {
-	isEmployeeEmailUsable,
-	isEmployeePhoneNumberUsable,
-	uniqueEmails,
-	uniquePhoneNumbers,
-} from "../../middlewares/employee.middleware";
+	importEmployees,
+	makeEmployee,
+	makeEmployees,
+} from "../../functions/modules/hris/post.hris.function";
+import {
+	applyBankAccountToEmployee,
+	updateBankAccountData,
+	updateEmployeeData,
+	updateEmployeeEmploymentStatus,
+	updateEmploymentHistoryData,
+} from "../../functions/modules/hris/patch.hris.function";
+
 const hris = express.Router();
 
-hris.post("/import-employees", uploadCsv.single("csv"), async (req, res) => {
-	const company = req.userAuthCreds.company.benefits;
+/* 
+	Get requests
+*/
+hris.get("/get/employees", async (req, res) => {
+	const company = req.userAuthCreds.company;
+	try {
+		const {status, data} = await getEmployees(company);
+		res.status(status).json(data);
+	} catch (error: any) {
+		res.status(500).json({error: error.message});
+	}
+});
+hris.get("/get/employee/:employeeUuid", async (req, res) => {
+	const company = req.userAuthCreds.company;
+	const {employeeUuid} = req.params;
+
+	try {
+		const {status, data} = await getEmployee(company, employeeUuid);
+		res.status(status).json(data);
+	} catch (error: any) {
+		res.status(500).json({error: error.message});
+	}
+});
+hris.get("/find/employee", async (req, res) => {
+	const company = req.userAuthCreds.company;
+	const keyword = req.query.keyword;
+
+	try {
+		const {status, data} = await findEmployee(company, keyword);
+		res.status(status).json(data);
+	} catch (error: any) {
+		res.status(500).json({error: error.message});
+	}
+});
+hris.get("/get/OrgChart", async (req, res) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({error: error.message});
+	}
+});
+/* 
+	Get requests
+*/
+
+/* 
+	Post requests
+*/
+
+hris.post("/import/employees", async (req, res) => {
+	const company = req.userAuthCreds.company;
 	const csvBuffer = req.file?.buffer.toString("utf8");
 
 	if (!csvBuffer) {
 		res.status(400).json({error: "please provide a csv"});
 	} else {
 		try {
-			const result = await employeesCsvToJsonArray(csvBuffer, company);
-			res.status(result.status).json(result.data);
+			const {status, data} = await importEmployees(company, csvBuffer);
+			res.status(status).json(data);
 		} catch (error: any) {
 			res.status(500).json({error: error.message});
 		}
 	}
 });
-
-hris.post(
-	"/onboard-employee",
-	isEmployeeEmailUsable,
-	isEmployeePhoneNumberUsable,
-	async (req, res) => {
-		const company = req.userAuthCreds.company;
-		try {
-			const result = await onboardEmployee(req.body, company);
-			res.status(result.status).json(result.data);
-		} catch (error: any) {
-			res.status(500).json({error: error.message});
-		}
-	},
-);
-
-hris.post("/onboard-employees", uniqueEmails, uniquePhoneNumbers, async (req, res) => {
+hris.post("/make/employees", async (req, res) => {
 	const company = req.userAuthCreds.company;
+	const {body} = req.body;
+
 	try {
-		const result = await onBoardEmployees(req.body, company);
-		res.status(result.status).json(result.data);
+		const {status, data} = await makeEmployees(company, body);
+		res.status(status).json(data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
 });
+hris.post("/make/employee", async (req, res) => {
+	const company = req.userAuthCreds.company;
+	const {body} = req.body;
 
-hris.patch("/offboard-employee/:employeeUuid", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
+	try {
+		const {status, data} = await makeEmployee(company, body);
+		res.status(status).json(data);
+	} catch (error: any) {
+		res.status(500).json({error: error.message});
+	}
+});
+/* 
+	Post requests
+*/
+
+/* 
+	Patch requests
+*/
+
+hris.patch("/update/offboardEmployee/:employeeUuid", async (req, res) => {
+	const company = req.userAuthCreds.company;
 	const {employeeUuid} = req.params;
+	const {body} = req.body;
+
 	try {
-		const result = await offboardEmployee(req.body, employeeUuid, company);
-		res.status(result.status).json(result.data);
+		const {status, data} = await updateEmployeeEmploymentStatus(company, employeeUuid, body);
+		res.status(status).json(data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
 });
-
-hris.patch("/assign-bank-account/:employeeUuid", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
+hris.patch("/update/assignBankAccountToEmployee/:employeeUuid", async (req, res) => {
+	const company = req.userAuthCreds.company;
 	const {employeeUuid} = req.params;
+	const {body} = req.body;
+
 	try {
-		const result = await assignBankAccount(req.body, employeeUuid, company);
-		res.status(result.status).json(result.data);
+		const {status, data} = await applyBankAccountToEmployee(company, employeeUuid, body);
+		res.status(status).json(data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
 });
-
-hris.get("/employees", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
-	try {
-		const result = await employees(company);
-		res.status(result.status).json(result.data);
-	} catch (error: any) {
-		res.status(500).json({error: error.message});
-	}
-});
-
-hris.get("/search-employee", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
-	const keyword = req.query.keyword;
-	try {
-		const result = await searchEmployee(company, keyword);
-		res.status(result.status).json(result.data);
-	} catch (error: any) {
-		res.status(500).json({error: error.message});
-	}
-});
-
-hris.get("/employee/:employeeUuid", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
+hris.patch("/update/employee/:employeeUuid", async (req, res) => {
+	const company = req.userAuthCreds.company;
 	const {employeeUuid} = req.params;
+	const {body} = req.body;
+
 	try {
-		const result = await employee(company, employeeUuid);
-		res.status(result.status).json(result.data);
+		const {status, data} = await updateEmployeeData(company, employeeUuid, body);
+		res.status(status).json(data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
 });
-
-hris.get("/org-chart-tree/:employeeUuid", async (req, res) => {
-	const {employeeUuid} = req.params;
-	try {
-		const result = await orgChartTree(employeeUuid);
-		res.status(result.status).json(result.data);
-	} catch (error: any) {
-		res.status(500).json({error: error.message});
-	}
-});
-
-hris.patch("/update-employee/:employeeUuid", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
-	const {employeeUuid} = req.params;
-	try {
-		const result = await updateEmployee(req.body, company, employeeUuid);
-		res.status(result.status).json(result.data);
-	} catch (error: any) {
-		res.status(500).json({error: error.message});
-	}
-});
-
-hris.patch("/update-employment-history/:employmentHistoryUuid", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
+hris.patch("/update/employmentHistory/:employmentHistoryUuid", async (req, res) => {
+	const company = req.userAuthCreds.company;
 	const {employmentHistoryUuid} = req.params;
-	try {
-		const result = await updateEmploymentHistory(req.body, company, employmentHistoryUuid);
-		res.status(result.status).json(result.data);
-	} catch (error: any) {
-		res.status(500).json({error: error.message});
-	}
-});
+	const {body} = req.body;
 
-hris.patch("/update-bank-account/:bankAccountUuid", async (req, res) => {
-	const company = req.userAuthCreds.company.uuid;
-	const {bankAccountUuid} = req.params;
 	try {
-		const result = await updateBankAccount(req.body, company, bankAccountUuid);
-		res.status(result.status).json(result.data);
+		const {status, data} = await updateEmploymentHistoryData(company, employmentHistoryUuid, body);
+		res.status(status).json(data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
 });
+hris.patch("/update/bankAccount/:bankAccountUuid", async (req, res) => {
+	const company = req.userAuthCreds.company;
+	const {bankAccountUuid} = req.params;
+	const {body} = req.body;
+
+	try {
+		const {status, data} = await updateBankAccountData(company, bankAccountUuid, body);
+		res.status(status).json(data);
+	} catch (error: any) {
+		res.status(500).json({error: error.message});
+	}
+});
+/* 
+	Patch requests
+*/
 
 export default hris;
