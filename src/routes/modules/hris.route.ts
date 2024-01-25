67c9bc9ed84fbf973fcb8866c703ccd44529970a
dev-dionmar <dionmarl@campoteltechnologies.com>
2024-01-25 17:43:17 +0800

    fix req body

--- src/routes/modules/hris.route.ts
+++ src/routes/modules/hris.route.ts
@@ -3,6 +3,7 @@ import {
 	findEmployee,
 	getEmployee,
 	getEmployees,
+	getOrgChart,
 } from "../../functions/modules/hris/get.hris.function";
 import {
 	importEmployees,
@@ -16,6 +17,13 @@ import {
 	updateEmployeeEmploymentStatus,
 	updateEmploymentHistoryData,
 } from "../../functions/modules/hris/patch.hris.function";
+import {uploadCsv} from "../../middlewares/multer.middleware";
+import {
+	isEmployeeEmailUsable,
+	isEmployeePhoneNumberUsable,
+	uniqueEmails,
+	uniquePhoneNumbers,
+} from "../../middlewares/employee.middleware";
 
 const hris = express.Router();
 
@@ -53,8 +61,11 @@ hris.get("/find/employee", async (req, res) => {
 		res.status(500).json({error: error.message});
 	}
 });
-hris.get("/get/org-chart", async (req, res) => {
+hris.get("/get/org-chart/:employeeUuid", async (req, res) => {
+	const {employeeUuid} = req.params;
 	try {
+		const {status, data} = await getOrgChart(employeeUuid);
+		res.status(status).json(data);
 	} catch (error: any) {
 		res.status(500).json({error: error.message});
 	}
@@ -67,7 +78,7 @@ hris.get("/get/org-chart", async (req, res) => {
 	Post requests
 */
 
-hris.post("/import/employees", async (req, res) => {
+hris.post("/import/employees", uploadCsv.single("csv"), async (req, res) => {
 	const company = req.userAuthCreds.company;
 	const csvBuffer = req.file?.buffer.toString("utf8");
 
@@ -82,9 +93,9 @@ hris.post("/import/employees", async (req, res) => {
 		}
 	}
 });
-hris.post("/make/employees", async (req, res) => {
+hris.post("/make/employees", uniqueEmails, uniquePhoneNumbers, async (req, res) => {
 	const company = req.userAuthCreds.company;
-	const {body} = req.body;
+	const {body} = req;
 
 	try {
 		const {status, data} = await makeEmployees(company, body);
@@ -93,17 +104,22 @@ hris.post("/make/employees", async (req, res) => {
 		res.status(500).json({error: error.message});
 	}
 });
-hris.post("/make/employee", async (req, res) => {
-	const company = req.userAuthCreds.company;
-	const {body} = req.body;
+hris.post(
+	"/make/employee",
+	isEmployeeEmailUsable,
+	isEmployeePhoneNumberUsable,
+	async (req, res) => {
+		const company = req.userAuthCreds.company;
+		const {body} = req;
 
-	try {
-		const {status, data} = await makeEmployee(company, body);
-		res.status(status).json(data);
-	} catch (error: any) {
-		res.status(500).json({error: error.message});
-	}
-});
+		try {
+			const {status, data} = await makeEmployee(company, body);
+			res.status(status).json(data);
+		} catch (error: any) {
+			res.status(500).json({error: error.message});
+		}
+	},
+);
 /* 
 	Post requests
 */
@@ -115,7 +131,7 @@ hris.post("/make/employee", async (req, res) => {
 hris.patch("/update/offboard-employee/:employeeUuid", async (req, res) => {
 	const company = req.userAuthCreds.company;
 	const {employeeUuid} = req.params;
-	const {body} = req.body;
+	const {body} = req;
 
 	try {
 		const {status, data} = await updateEmployeeEmploymentStatus(company, employeeUuid, body);
@@ -127,7 +143,7 @@ hris.patch("/update/offboard-employee/:employeeUuid", async (req, res) => {
 hris.patch("/update/assign-bank-account-to-employee/:employeeUuid", async (req, res) => {
 	const company = req.userAuthCreds.company;
 	const {employeeUuid} = req.params;
-	const {body} = req.body;
+	const {body} = req;
 
 	try {
 		const {status, data} = await applyBankAccountToEmployee(company, employeeUuid, body);
@@ -139,7 +155,7 @@ hris.patch("/update/assign-bank-account-to-employee/:employeeUuid", async (req, 
 hris.patch("/update/employee/:employeeUuid", async (req, res) => {
 	const company = req.userAuthCreds.company;
 	const {employeeUuid} = req.params;
-	const {body} = req.body;
+	const {body} = req;
 
 	try {
 		const {status, data} = await updateEmployeeData(company, employeeUuid, body);
@@ -151,7 +167,7 @@ hris.patch("/update/employee/:employeeUuid", async (req, res) => {
 hris.patch("/update/employment-history/:employmentHistoryUuid", async (req, res) => {
 	const company = req.userAuthCreds.company;
 	const {employmentHistoryUuid} = req.params;
-	const {body} = req.body;
+	const {body} = req;
 
 	try {
 		const {status, data} = await updateEmploymentHistoryData(company, employmentHistoryUuid, body);
@@ -163,7 +179,7 @@ hris.patch("/update/employment-history/:employmentHistoryUuid", async (req, res)
 hris.patch("/update/bank-account/:bankAccountUuid", async (req, res) => {
 	const company = req.userAuthCreds.company;
 	const {bankAccountUuid} = req.params;
-	const {body} = req.body;
+	const {body} = req;
 
 	try {
 		const {status, data} = await updateBankAccountData(company, bankAccountUuid, body);
